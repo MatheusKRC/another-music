8a03307f7079a5173353b9bb6c82fd038ea4948b
Matheus leão <kyracraft0@gmail.com>
2022-05-26 16:32:55 -0300

    [Requisito 5] criado input de pesquisa de artista

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,39 @@ import React from 'react';
 import Header from '../Components/Header';
 
 class Search extends React.Component {
+  state = {
+    artist: '',
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState(() => ({
+      [name]: value,
+    }));
+  }
+
   render() {
+    const { artist } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            name="artist"
+            type="text"
+            placeholder="Nome Do Artista"
+            data-testid="search-artist-input"
+            onChange={ this.handleChange }
+          />
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            disabled={ artist.length <= 1 }
+          >
+            Pesquisar
+
+          </button>
+        </form>
       </div>
     );
   }
