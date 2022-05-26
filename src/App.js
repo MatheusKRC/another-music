ab98004c8b25db5ecfe1f058b2d6f640b4dbdf1c
Matheus leão <kyracraft0@gmail.com>
2022-05-25 23:01:30 -0300

    [Requisito 1] criadas rotas e componenets

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Content from './Components/Content';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <div>
+        <Content />
+      </div>
+    );
   }
 }
 
