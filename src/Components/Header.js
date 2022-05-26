9960d723fc7be153bab67957aef280802c906bf5
Matheus leão <kyracraft0@gmail.com>
2022-05-26 16:26:45 -0300

    [Requisito 4] criados links de navegação no header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import Loading from '../pages/Loading';
 import { getUser } from '../services/userAPI';
 
@@ -23,7 +24,15 @@ class Header extends React.Component {
       return (
         <header data-testid="header-component">
           {loading ? (<Loading />) : (
-            <h3 data-testid="header-user-name">{user}</h3>)}
+            <div>
+
+              <h3 data-testid="header-user-name">{user}</h3>
+              <Link data-testid="link-to-search" to="/search" />
+              <Link data-testid="link-to-favorites" to="/favorites" />
+              <Link data-testid="link-to-profile" to="/profile" />
+
+            </div>
+          )}
 
         </header>
       );
