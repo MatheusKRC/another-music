7b241e34ea02f40c33b7af271c77e3a9ce36b8b2
Matheus leão <kyracraft0@gmail.com>
2022-05-26 15:45:38 -0300

    [Requisito 2] criada função salvar informações e abrir página do search

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,61 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends React.Component {
+  state = {
+    user: '',
+    loading: false,
+    loaded: false,
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState(() => ({
+      [name]: value,
+    }));
+  }
+
+  handleSaveUser = async () => {
+    const { user } = this.state;
+
+    this.setState({ loading: true });
+
+    await createUser({ name: user });
+
+    this.setState({ loaded: true });
+  }
+
   render() {
+    const { loading, user, loaded } = this.state;
     return (
-      <div data-testid="page-login" />
+      <div>
+        {loaded && <Redirect to="/search" />}
+        {loading ? (<Loading />) : (
+          <div data-testid="page-login">
+            <input
+              name="user"
+              data-testid="login-name-input"
+              type="text"
+              placeholder="Nome"
+              onChange={ this.handleChange }
+            />
+
+            <button
+              type="button"
+              data-testid="login-submit-button"
+              disabled={ user.length <= 2 }
+              onClick={ this.handleSaveUser }
+            >
+              Entrar
+
+            </button>
+
+          </div>) }
+
+      </div>
+
     );
   }
 }
