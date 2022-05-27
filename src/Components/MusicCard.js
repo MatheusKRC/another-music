0dd62bd61509b5c8d37982c5f91b0b98d8793d0b
Matheus leão <kyracraft0@gmail.com>
2022-05-26 23:41:57 -0300

    [Requisito 7] criada requisição de musicas dos albuns e listagem de musicas

---
+++ src/Components/MusicCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MusicCard extends React.Component {
+  render() {
+    const { music } = this.props;
+    return (
+      <div>
+        <h3>{music.trackName}</h3>
+        <audio data-testid="audio-component" src={ music.previewUrl } controls>
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          {' '}
+          <code>audio</code>
+          .
+        </audio>
+      </div>
+    );
+  }
+}
+
+export default MusicCard;
+
+MusicCard.propTypes = {
+  music: PropTypes.shape({
+    trackName: PropTypes.string.isRequired,
+    previewUrl: PropTypes.string.isRequired,
+  }).isRequired,
+};
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,14 +1,59 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../Components/Header';
+import MusicCard from '../Components/MusicCard';
+import getMusics from '../services/musicsAPI';
 
 class Album extends React.Component {
+  state = {
+    musics: [],
+    collection: [],
+  }
+
+  componentDidMount() {
+    this.pushMusic();
+  }
+
+  pushMusic = async () => {
+    const { match: { params: { id } } } = this.props;
+    const pushMusics = await getMusics(id);
+    const listMusics = pushMusics.filter((_music, index) => index !== 0);
+    this.setState({ musics: pushMusics,
+      collection: listMusics });
+  }
+
   render() {
+    const { collection, musics } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
-      </div>
-    );
+        {musics.length > 0 && (
+          <div>
+            <h3 data-testid="artist-name">
+              {musics[0].artistName}
+            </h3>
+
+            <h4 data-testid="album-name">
+              {musics[0].collectionName}
+            </h4>
+          </div>
+
+        )}
+
+        <div>
+          {collection.map((music, index) => (
+            <div key={ index }>
+              <MusicCard music={ music } />
+            </div>
+          ))}
+        </div>
+      </div>);
   }
 }
 
+Album.propTypes = {
+  match: PropTypes.shape.isRequired,
+  params: PropTypes.shape.isRequired,
+  id: PropTypes.string.isRequired,
+}.isRequired;
 export default Album;
